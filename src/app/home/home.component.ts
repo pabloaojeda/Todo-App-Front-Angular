735f831568f3d02577868db183e966a069c756b6
pabloaojeda <pabloaojeda@gmail.com>
2021-11-21 16:07:42 -0300

    HomeComponent Design and logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoComponent } from '../todo/todo.component';
+import { ApiService } from '../services/api.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  todos: any = [];
+  filterTodos: any[] = [
+    { title: 'Test', description: 'Test description', status: 'OPEN' },
+  ];
 
-  constructor() { }
+  constructor(private ApiService: ApiService, private dialog: MatDialog) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  filterChanged(ev: MatSelectChange) {
+    const value = ev.value;
+    this.filterTodos = this.todos;
+    if (value) {
+      this.filterTodos = this.filterTodos.filter((t) => t.status === value);
+      console.log(this.filterTodos);
+    } else {
+      this.filterTodos = this.todos;
+    }
   }
 
+  openDialog() {
+    const dialogRef = this.dialog.open(TodoComponent, {
+      width: '500px',
+      hasBackdrop: true,
+      role: 'dialog',
+      height: '500px',
+    });
+  }
+
+  statusChanged(ev: MatSelectChange, todoId: number, index: number) {
+    const value = ev.value;
+  }
+
+  delete(id: number) {
+    if (confirm('Do you want to remove this Todo ?')) {
+    }
+  }
 }
